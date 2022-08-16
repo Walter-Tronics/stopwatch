d83522ec1adf138bee84a282a86f2ba71b865ac3
Walter Tronics <waltertronics02@gmail.com>
2022-08-16 21:41:23 +0100

    Adding a windows load function and commenting

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var hour = 00;
-var min = 00;
-var sec = 00;
-var ms = 00;
-var start = document.querySelector('#start');
-var stop = document.querySelector('#stop');
-var reset = document.querySelector('#reset');
-var interval;
+window.onload=()=>{
+
+//setting variables
+let hour = 00,
+min = 00,
+sec = 00,
+ms = 00,
+start = document.querySelector('#start'),
+stop = document.querySelector('#stop'),
+reset = document.querySelector('#reset'),
+interval;
+
+//setting the count function
 function count(){
+    //incrementing the miliseconds
     ms++;
+    //if the miliseconds are greater than 99, reset to 0 and increment the seconds
     if (ms==100) {
+        //incrementing the seconds
         sec++;
+        //reset the miliseconds to 0
         ms=0;
     }
+    //if the seconds are greater than 59, reset to 0 and increment the minutes
     if (sec==60) {
+        //incrementing the minutes
         min++;
+        //reset the seconds to 0
         sec=0;
     }
+    //if the minutes are greater than 59, reset to 0 and increment the hours
     if (min==60) {
+        //incrementing the hours
         hour++;
+        //reset the minutes to 0
         min=0;
     }
     if (ms<10) {ms="0"+ms;}
@@ -43,4 +58,5 @@ reset.onclick = function(){
     document.querySelector('#sec').innerHTML=sec;
     document.querySelector('#min').innerHTML=min;
     document.querySelector('#hour').innerHTML=hour;
+}
 }
