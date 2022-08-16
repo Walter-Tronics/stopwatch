838a1309d707682b45fae9aa53e99031b0bdfcb2
Walter Tronics <waltertronics02@gmail.com>
2022-08-16 21:52:48 +0100

    commenting...

--- index.js
+++ index.js
@@ -35,25 +35,36 @@ function count(){
         //reset the minutes to 0
         min=0;
     }
+    //if miliseconds are less than 10, add a 0 to the front of the string
     if (ms<10) {ms="0"+ms;}
+    //append the miliseconds, seconds, minutes and hours to the html
     document.querySelector('#count').innerHTML=ms;
     document.querySelector('#sec').innerHTML=sec;
     document.querySelector('#min').innerHTML=min;
     document.querySelector('#hour').innerHTML=hour;
 }
+//start the count function
 start.onclick = function(){
+    //clear the interval if it is already running
     clearInterval(interval);
+    //set the interval to the count function
     interval = setInterval(count,10);
 }
+//stop the count function
 stop.onclick = function(){
+    //clear the interval
     clearInterval(interval);
 }
+//reset the count function
 reset.onclick = function(){
+    //clear the interval
     clearInterval(interval);
+    //reset the variables to 0
     hour=0;
     min=0;
     sec=0;
     ms=0;
+    //append the variables to the html
     document.querySelector('#count').innerHTML=ms;
     document.querySelector('#sec').innerHTML=sec;
     document.querySelector('#min').innerHTML=min;
